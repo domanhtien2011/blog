a71e72cb0bb7e6a41d63a5539c5639147f9f2d86
domanhtien2011 <domanhtien2011@gmail.com>
2015-12-24 19:02:24 +0700

    Add New Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,34 @@
 class PostsController < ApplicationController
-  def index
+  before_action :find_post , only: [:show, :edit, :update, :destroy]
 
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
 
+    if @post.save
+      redirect_to(@post)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
